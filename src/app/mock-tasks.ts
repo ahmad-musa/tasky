351c6316b4e8963e24f90aad5a4ffb193a0871b3
ahmad-musa <ahmadmusa0987@gmail.com>
2022-04-29 01:56:09 +0600

    MOCK TASK SHOWING

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,18 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Appointment with Dentist",
+        date: "April 28 at 5.00 pm",
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: "Business meeting",
+        date: "April 30 at 12.00 pm",
+        reminder: true,
+    }
+
+]
